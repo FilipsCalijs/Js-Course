e7f318929dfedcae8f8d7320030f8fb2ed341f21
FilipsCalijs <filipscalijs@gmail.com>
2023-10-21 19:17:08 +0300

    done with 84th lesson

--- 84_lesson/js/script.js
+++ 84_lesson/js/script.js
@@ -206,35 +206,51 @@ window.addEventListener('DOMContentLoaded', function() {
     ).render();
 
     //Forms
-
     const forms = document.querySelectorAll('form');
-
     const message = {
-        loading: 'загрузка',
-        success: 'спасибо',
-        failure: 'что то пошло не так',
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
     };
 
-    function postData(form){
-        form.addEventListener('sumbit', (e) =>{
-            e.preventDefault()
-            
-            const statusMessage = createElement('div');
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
             statusMessage.classList.add('status');
-
-            const requst = new XMLHttpRequest();
-            requst.open('POST', 'server');
-
+            statusMessage.textContent = message.loading;
+            form.appendChild(statusMessage);
+        
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
             const formData = new FormData(form);
 
-            requst.send(formData);
+            const object = {};
+            formData.forEach(function(value, key){
+                object[key] = value;
+            });
+            const json = JSON.stringify(object);
 
-            requst.addEventListener('load', () => {
-                if (requst.status === 200){
-                    console.log(requst.response);
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
                 }
-            })
+            });
         });
     }
-
 });
